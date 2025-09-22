8adacf1d7fd20ad7a4ab89887a3f2cb2191cb2a3
Valdir <valdir.d.silva.junior@gmail.com>
2025-09-22 06:36:11 -0300

    Ajuste prescaler TIM15 para 8162

--- CNC_Controller/Core/Src/tim.c
+++ CNC_Controller/Core/Src/tim.c
@@ -246,7 +246,7 @@ void MX_TIM15_Init(void)
 
   /* USER CODE END TIM15_Init 1 */
   htim15.Instance = TIM15;
-  htim15.Init.Prescaler = 0;
+  htim15.Init.Prescaler = 8191;
   htim15.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim15.Init.Period = 65535;
   htim15.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
