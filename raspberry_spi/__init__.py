6ff1bfe1ac3b6f622fd916a1e56e870beb4189aa
Valdir <valdir.d.silva.junior@gmail.com>
2025-10-27 08:24:15 -0300

    Adicionando o projeto com acesso global

---
+++ raspberry_spi/__init__.py
@@ -0,0 +1,8 @@
+"""Pacote de utilitários para comunicação SPI e CLI do TMC5160.
+
+Subcomandos principais disponíveis via console script:
+ - tmc5160-cli set|status|get|errors|security
+"""
+
+__all__ = []
+
